d7624cf27a68e5e37da95e7db656277477f74dfe
Nitikon Thrrmnithikhun <Lahu001710@gmail.com>
2021-06-13 12:48:31 +0700

    เพิ่มกิจกรรม

---
+++ resources/views/user/hello.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.master' )
+@section('title' , 'สวัสดีคุณ')
+@section('content')
+<h1>สวัสดีคุณ</h1>
+<hr>
+
+สวัสดีคุณ {{$name}} {{$surname}} {{$age}}
+@endsection
---
+++ resources/views/user/index1.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.master' )
+@section('title' , 'ใช้งาน')
+@section('content')
+<h1>ผู้ใช้งาน</h1>
+@endsection
